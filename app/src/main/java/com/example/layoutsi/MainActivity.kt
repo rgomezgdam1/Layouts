50c6d94e24eb07059288d25c7a310f7121ea854a
rgomezgdam1 <rgomezgdam1@gmail.com>
2024-01-11 13:58:53 +0100

    first commit, primer layout

---
+++ app/src/main/java/com/example/layoutsi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.layoutsi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.frame_layout)
+    }
+}
